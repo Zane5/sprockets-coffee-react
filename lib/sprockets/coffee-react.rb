88065c54ebe937e53246f625ad18fdb1db4c881a
Zane5 <Zane5@users.noreply.github.com>
2016-03-11 18:40:50 +0800

    undefined method `register_engine'
    /sprockets/coffee-react/engine.rb:20:in `configure_env': undefined method `register_engine' for #<Sprockets::Environment:0xa480ae4>

--- lib/sprockets/coffee-react.rb
+++ lib/sprockets/coffee-react.rb
@@ -23,8 +23,8 @@ module Sprockets
     def self.install(environment = ::Sprockets)
       environment.register_preprocessor 'application/javascript', Sprockets::CoffeeReact
       environment.register_postprocessor 'application/javascript', Sprockets::CoffeeReactPostprocessor
-      environment.register_engine '.cjsx', Sprockets::CoffeeReactScript
-      environment.register_engine '.js.cjsx', Sprockets::CoffeeReactScript
+      environment.register_pipeline '.cjsx', Sprockets::CoffeeReactScript
+      environment.register_pipeline '.js.cjsx', Sprockets::CoffeeReactScript
     end
   end
 end
